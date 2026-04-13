5c18a783933c1a4cffb3d166a07a8d7113ce6213
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-13 09:21:35 -0300

    Add files via upload

---
+++ aula04.java
@@ -0,0 +1,8 @@
+int items = 50;
+float costPerItem =  9.99f;
+float totalCost = Items * costPerItem;
+char currency = '$';
+
+System.out.println("Number of items:" + items);
+System.out.println("Cost per item: " + CostPerItem + currency);
+System.out.println("Total cost = " + totalcost + currency);
