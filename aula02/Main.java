3aa9b10153771011d65163b7672824c959d9b048
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-14 22:49:48 -0300

    Add files via upload

---
+++ aula02/Main.java
@@ -0,0 +1,17 @@
+package aula02;
+
+public class Main{
+    public static void main(String [] args) {
+     String msgPt1="olá";
+     String msgPt2="alunos";
+     String msgPt3="a soma dos numeros";
+     int n1= 18;
+     int n2= 22;
+
+     System.out.println(msgPt1.concat(msgPt2));
+     System.out.println(msgPt3+(n1+n2));
+
+
+}
+
+}
