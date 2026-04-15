dcba4aac77b9c883332570b0016c8a139ddb125f
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-14 22:21:36 -0300

    Delete aula04.java

--- aula04.java
+++
@@ -1,8 +0,0 @@
-int items = 50;
-float costPerItem =  9.99f;
-float totalCost = Items * costPerItem;
-char currency = '$';
-
-System.out.println("Number of items:" + items);
-System.out.println("Cost per item: " + CostPerItem + currency);
-System.out.println("Total cost = " + totalcost + currency);
