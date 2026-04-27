ca5da93af9dff5318a2d9a8e95896a0121b66afe
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-27 16:46:19 -0300

    Add files via upload

---
+++ aula05/Narrowing.java
@@ -0,0 +1,13 @@
+package aula05;
+
+public class Narrowing {
+    public static void main(String[] args){
+        int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+
+    }
+    
+}
