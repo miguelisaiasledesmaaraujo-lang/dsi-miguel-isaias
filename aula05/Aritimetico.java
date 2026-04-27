2b7f22014adbca7fde4c3721a236027a071f248d
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-27 15:33:39 -0300

    Add files via upload

---
+++ aula05/Aritimetico.java
@@ -0,0 +1,23 @@
+package aula05;
+
+public class Aritimetico {
+    public static void main(String[] args){
+    int x = 105;
+    int y = 500;
+
+    System.out.println(x + y); 
+    System.out.println(x - y); 
+    System.out.println(x * y); 
+    System.out.println(x / y); 
+    System.out.println(x % y);
+
+    int z = 501;
+    ++z;
+    System.out.println(z); 
+    --z;
+    System.out.println(z);
+  }
+}
+
+    
+
