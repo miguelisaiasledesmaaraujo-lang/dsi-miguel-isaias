6972a249d12e07979558b77cdf7768751f2c7bef
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-04-29 16:22:01 -0300

    Add files via upload

---
+++ aula05/Widening.java
@@ -0,0 +1,13 @@
+package aula05;
+
+public class Widening{
+    public static void main(String[] args){
+      
+        int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  
+    }
+}
