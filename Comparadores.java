78cd85c73da11802151810e75bed89235569dc47
miguelisaiasledesmaaraujo-lang <miguelisaiasledesmaaraujo@gmail.com>
2026-05-04 09:27:17 -0300

    Add files via upload

---
+++ Comparadores.java
@@ -0,0 +1,28 @@
+package aula06;
+
+public class Comparadores{
+    public static void main(String[] args){
+
+        boolean estaTrancendendo = true;
+
+        if (estaTrancendendo){
+            System.out.println("realmente, está transcendendo a materia");
+
+
+
+        int y = 420;
+        int z = 670;
+        
+         if (z > y){
+            System.out.println("z é mais daora que y");
+
+            int aa = 667;
+            int bb = 667;
+        
+           if (aa == bb) {
+            System.out.println("ambos são suspeitos")
+           }
+         }
+        } 
+    }
+}
